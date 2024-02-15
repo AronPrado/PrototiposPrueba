2f3cb43c7bd455d2a9cc78e73a1cbbb58dcfbdd9
Agust <agustinmorillos3@gmail.com>
2024-02-14 23:14:43 -0500

    Solo agrege un Activity

---
+++ app/src/main/java/com/javierprado/prototipos/PruebaAgus.kt
@@ -0,0 +1,11 @@
+package com.javierprado.prototipos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PruebaAgus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba_agus)
+    }
+}
